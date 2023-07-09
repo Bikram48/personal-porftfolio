217de7708cdfee2b5bb06cd74206af8d0025b686
Bikram48 <chandbikram001@gmail.com>
2023-07-09 21:57:00 +0545

    hero component for mobile devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
-
   return (
-    <Navbar />
-  )
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 }
 
-export default App
+export default App;
