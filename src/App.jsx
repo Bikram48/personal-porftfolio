59e002067aa7e690adb3e1c9d68cadbc148bc1bf
Bikram48 <chandbikram001@gmail.com>
2023-07-09 15:32:44 +0545

    tailwind post css configuration with vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="bg-red">
+      <h1 className='bg-red text-6xl'>Hello World</h1>
+      <button class="bg-sky-500">Hello</button>
+     </div>
+    </>
+  )
+}
+
+export default App
