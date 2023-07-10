43d6ed6f25a4e999bbf35c003537f13b31954c4d
Bikram48 <chandbikram001@gmail.com>
2023-07-10 15:00:25 +0545

    add - skills component

---
+++ src/components/Skill.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { css, html, js, react, tailwind } from "./imports.js";
+
+export default function Skill() {
+  return (
+    <div className="bg-body text-white p-6 md:p-20">
+      <div className="flex mt-6 items-center">
+        <div className="w-4 h-1 bg-[#de1054] mr-4"></div>
+        <h2 className="text-3xl">Skills</h2>
+      </div>
+      <div className="mt-6 md:flex md:justify-between md:items-center md:mt-11">
+        <h3 className="hidden md:block text-xl basis-2/4">FrontEnd</h3>
+        <div className="grid grid-cols-3 gap-10 basis-2/4">
+          <div className="flex flex-col items-center w-1/2">
+            <img src={react} />
+            <p className="text-white mt-4 text-xl font-bold">React</p>
+          </div>
+          <div className="flex flex-col items-center w-1/2">
+            <img src={css} />
+            <p className="text-white mt-4 text-xl font-bold">CSS</p>
+          </div>
+          <div className="flex flex-col items-center w-1/2">
+            <img src={js} />
+            <p className="text-white mt-4 text-xl font-bold">JavaScript</p>
+          </div>
+          <div className="flex flex-col items-center w-1/2">
+            <img src={tailwind} />
+            <p className="text-white mt-4 text-xl font-bold">TailWind</p>
+          </div>
+          <div className="flex flex-col items-center w-1/2">
+            <img className="w-1/2" src={html} />
+            <p className="text-white mt-4 text-xl font-bold">HTML</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/imports.js
@@ -0,0 +1,7 @@
+import css from "../assets/css.png";
+import html from "../assets/html5.png";
+import js from "../assets/js.png";
+import react from "../assets/react.png";
+import tailwind from "../assets/tailwind.png";
+
+export { css, html, js, react, tailwind };
